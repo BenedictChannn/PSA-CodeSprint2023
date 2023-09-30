086eaacfab8c02920de77b50a43d676d32d9f859
Joey Lee <joeylee@comp.nus.edu>
2023-09-30 10:58:10 +0800

    Move folders to src

---
+++ internetwork/src/Welcome.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Welcome() {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  )
+}
+
